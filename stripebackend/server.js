c5d958e2f8ee4dd009252f2b5a74e6749136ca90
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-11-25 15:35:58 +0500

    initailze frontend and backend and install few libary

---
+++ stripebackend/server.js
@@ -0,0 +1,19 @@
+const cors = require("cors");
+const express = require("express");
+// TODE:add a stripe key
+const stripe = require("stripe")(process.env.PUBLISHABLE_KEY);
+const uuid = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+//middelware
+app.use(express.json());
+
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("server is working");
+});
+app.listen(PORT, () => {
+  console.log(`Server is runnig http://localhost:${PORT}/`);
+});
