c575b94de124028279b2804141916462ad7a98b3
sharozraees <52934734+sharozraees802@users.noreply.github.com>
2020-11-25 16:06:49 +0500

    make api is proper make no error

---
+++ stripebackend/routes/payment.routes.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const stripe = require("stripe")(process.env.PUBLISHABLE_KEY);
+const uuid = require("uuid");
+const router = express.Router();
+
+router.post("/payment", (req, res) => {
+  const { product, token } = req.body;
+  console.log("Product:", product);
+  console.log("price:", product.price);
+  const idempontencykey = uuid();
+
+  return stripe.customers
+    .create({
+      email: token.email,
+      source: token.id,
+    })
+    .then((customer) => {
+      stripe.charges.create(
+        {
+          amount: product.price * 100,
+          currency: "usd",
+          customer: customer.id,
+          receipt_email: token.email,
+          description: `purchase of ${product.name}`,
+          shipping: {
+            name: token.card.name,
+            address: {
+              country: token.card.address_country,
+            },
+          },
+        },
+        idempontencykey
+      );
+    })
+    .then((result) => res.send(200).json(result))
+    .catch((err) => console.log(err));
+});
+
+module.exports = router;
--- stripebackend/server.js
+++ stripebackend/server.js
@@ -11,6 +11,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use(cors());
+
+app.use(require("./routes/payment.routes"));
+
 app.get("/", (req, res) => {
   res.send("server is working");
 });
